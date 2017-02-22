cce8d998e0ab38fc8f592eb21064b34b6eec2ac9
frank_linux <liulei_frank@126.com>
2017-02-22 21:57:41 +0800

    Q371

--- Q371.py
+++ Q371.py
@@ -5,14 +5,32 @@ class Solution(object):
         :param b: int
         :return: int
         """
-        s = a ^ b
-        c = (a & b) << 1
-        while (0 != c):
-            temp = s
-            s = temp ^ c
-            c = (temp & c) << 1
-        return s
+        # if abs(a) == abs(b) and a * b < 0:
+        #     return 0
+        # s = a ^ b
+        # # print(bin(a))
+        # # print(bin(b))
+        # # print("s=>",s)
+        # c = (a & b) << 1
+        # # print("c=>",c)
+        # cnt = 0
+        # while (0 != c):
+        #     temp = s
+        #     print("temp=>",temp)
+        #     s = temp ^ c
+        #     print("sloop=>",s)
+        #     c = (temp & c) << 1
+        #     print("cloop=>",c)
+        #     if cnt == 10:
+        #         exit()
+        #     cnt += 1
+        # return s
+        while(b):
+            carry = a & b
+            a = a ^ b
+            b = carry << 1
+        return a
         pass
 
 if __name__ == '__main__':
-    print (Solution().getSum(-1, 1))
+    print (Solution().getSum(-14, 13))
